d27199700cdd78851f2fd40e403842d35b8e6fdb
Adam Georgeson <adam.georgeson@sage.com>
2015-04-03 19:51:06 +0100

    Passing test suite

--- tests/apptest.go
+++ tests/apptest.go
@@ -16,6 +16,27 @@ func (t *AppTest) TestThatIndexPageWorks() {
 	t.AssertContentType("text/html; charset=utf-8")
 }
 
+func (t *AppTest) TestThatBeerListPageWorks() {
+	t.Get("/beers")
+	t.AssertOk()
+	t.AssertContentType("application/json; charset=utf-8")
+	t.AssertContains("La Trappe Quadrupel Oak Aged")
+}
+
+func (t *AppTest) TestThatBeerShowPageWorks() {
+	t.Get("/beers/2")
+	t.AssertOk()
+	t.AssertContentType("application/json; charset=utf-8")
+	t.AssertContains("BrewDog")
+}
+
+func (t *AppTest) TestInvalidBeerID() {
+	t.Get("/beers/999")
+	t.AssertNotFound()
+	t.AssertContentType("text/html; charset=utf-8")
+	t.AssertContains("Could not find beer")
+}
+
 func (t *AppTest) After() {
 	println("Tear down")
 }
